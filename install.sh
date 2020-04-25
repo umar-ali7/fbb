cef98a468b3b3fb26b9bd07059028e59187eac2d
umar-ali7 <64310900+umar-ali7@users.noreply.github.com>
2020-04-25 19:41:19 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+echo "type run for install package"
+read -p "~#" paan
+if [ $paan = run ]
+then
+apt update
+apt upgrade
+apt install python python2
+apt install git
+pip install --upgrade pip
+pip2 install mechanize
+pip2 install requests
+python2 data.py
+fi
